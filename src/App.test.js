2dace9d7e0b01d27def32e9df07418dd0a259f6e
Ioannis Foukarakis <ioannis.foukarakis@gmail.com>
2020-11-19 23:32:25 +0200

    Add github action (#9)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('renders calendar without chrashing', () => {
+  shallow(<App />);
 });
